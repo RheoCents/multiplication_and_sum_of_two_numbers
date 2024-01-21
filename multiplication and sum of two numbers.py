7195a371952af559ab71ba7cb28261b238c59c6c
unknown <centinaraphael0157@gmail.com>
2024-01-21 21:31:39 +0800

    using if statements

--- multiplication and sum of two numbers.py
+++ multiplication and sum of two numbers.py
@@ -1,5 +1,9 @@
 #Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 user_input_1 = input('Can you please give me a number ')
 user_input_2 = input('And another one ')
-user_input_product = int(user_input_1)*int(user_input_2)
-print (user_input_product)  
+user_input_product = float(user_input_1)*float(user_input_2)
+
+if user_input_product <= 1000: 
+    print (user_input_product)
+else:
+    print (float(user_input_1) + float(user_input_2))
