67827be753984aa6d496245f88dea0ba12217395
unknown <centinaraphael0157@gmail.com>
2024-01-21 21:20:08 +0800

    upload test 2

---
+++ multiplication and sum of two numbers.py
@@ -0,0 +1,5 @@
+#Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+user_input_1 = input('Can you please give me a number ')
+user_input_2 = input('And another one ')
+user_input_product = {user_input_1} * {user_input_2}
+print (user_input_product) 
